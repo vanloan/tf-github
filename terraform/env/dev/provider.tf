96a1e7d1fcee9ae8afd81faa0ff3e5feea53d46c
vanloan <vanloan206@gmail.com>
2025-04-29 14:20:22 +0700

    Update provider.tf

--- terraform/env/dev/provider.tf
+++ terraform/env/dev/provider.tf
@@ -1,14 +1,19 @@
 terraform {
-  backend "s3" {
-    # profile = "test"
-    bucket  = "loanvt"
-    region  = "us-west-2"
-    key     = "dev/terraform.tfstate"
-    encrypt = true
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.67.0"
+    }
+
+    tls = {
+      source = "hashicorp/tls"
+      version = ">= 4.1.0"
+    }
   }
+
+  required_version = ">= 1.0"
 }
 
 provider "aws" {
   region = "us-west-2"
-  # profile = "test"
 }
